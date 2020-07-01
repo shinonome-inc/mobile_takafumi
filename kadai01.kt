061ec73f88dda1d13c404066a70a1e9ee15d3b93
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-07-01 19:27:29 +0900

    Revert "Update kadai01.kt"
    This reverts commit 4374433199f0dff3200245cc18cfc2045d5b6809.

--- kadai01.kt
+++ kadai01.kt
@@ -1,13 +1,13 @@
 # mobile_takafumi
-var numInt = 2000
+var Int = 2000
 
 fun main() {
    println("Jugde leap year program")
-   if(numInt % 400 == 0){
+   if(Int % 400 == 0){
        println("true")
-   }else if(numInt % 100 == 0){
+   }else if(Int % 100 == 0){
        println("false")
-   }else if(numInt % 4 == 0){
+   }else if(Int % 4 == 0){
        println("true")
    }else{
        println("false")
