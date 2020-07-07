b2dab3c3ed52c8bef52ab6eeaea642f89101e39a
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-07-07 19:15:39 +0900

    modified_3

--- src/kadai01.kt
+++ src/kadai01.kt
@@ -1,15 +1,18 @@
-var numInt = 2000
-
-fun main(args: Array<Int>) {
-   println("Jugde leap year program")
-   if(numInt % 400 == 0) {
-       println("true")
-   } else if(numInt % 100 == 0) {
-       println("false")
-   } else if(numInt % 4 == 0){
-       println("true")
-   }else{
-       println("false")
-   }
-
+fun numInt(a:Int) {
+    if (a % 400 == 0) {
+        println("true")
+    } else if (a % 100 == 0) {
+        println("false")
+    } else if (a % 4 == 0) {
+        println("true")
+    } else {
+        println("false")
+    }
 }
+
+fun main() {
+    println("閏年判定プログラム")
+
+    numInt(3000)
+}
+
