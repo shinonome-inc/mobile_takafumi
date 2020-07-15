21824077b54ffee0860dad93e229e38bcb9efb7e
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-07-15 21:18:52 +0900

    5th

--- src/kadai02.kt
+++ src/kadai02.kt
@@ -1,7 +1,7 @@
-enum class Hand{
-    Gu,
-    Ti,
-    Pa,
+enum class Hand(val id :Int){
+    Gu(0),
+    Ti(1),
+    Pa(2),
 }
 
 enum class Result{
@@ -10,39 +10,48 @@ enum class Result{
     Draw,
 }
 
-fun janken(x: Int) {
+fun janken(x: Hand) {
 
     val r = (0..2).random()
 
-    if(r == 0){
-        println("相手の手は${Hand.Gu}")
-    }else if(r == 1) {
-        println("相手の手は${Hand.Ti}")
-    }else {
-        println("相手の手は${Hand.Pa}")
+    when(r) {
+        0 -> {
+            println("相手の手は${Hand.Gu}")
+        }
+        1 -> {
+            println("相手の手は${Hand.Ti}")
+        }
+        2 -> {
+            println("相手の手は${Hand.Pa}")
+        }
     }
 
-    if(r == 0 && x == 0 || r == 1 && x == 1 || r ==2 && x == 2) {
+    if(r.equals(0)  && x.equals(0) || r.equals(1) && x.equals(1) || r.equals(2) && x.equals(2)) {
         println(Result.Draw)
-    }else if(r == 0 && x == 2 || r == 1 && x == 0 || r ==2 && x == 1){
-    println(Result.Win)
-    }else if(r == 0 && x == 1 || r == 1 && x == 2 || r ==2 && x == 0){
+    }else if(r.equals(0) && x.equals(2) || r.equals(1) && x.equals(0) || r.equals(2) && x.equals(1)){
+        println(Result.Win)
+    }else if(r.equals(0) && x.equals(1) || r.equals(1) && x.equals(2) || r.equals(2) && x.equals(0)){
         println(Result.Lose)
     } else {
         println("正しく入力してください")
     }
 }
 
-
 fun main() {
     println("ジャンケンプログラム")
 
     print("あなたの出す手を入力してください(グーは０、チョキは１、パーは２）：")
 
-    val x = readLine()?.toInt()
+    var x = readLine()?.toInt()
+    var y:Hand = Hand.Gu
 
-    if(x !=null){
-        janken(x)
+    if(x == 1){
+        y = Hand.Ti
+
+    }else if(x == 2){
+        y = Hand.Pa
     }
+
+    janken(y)
 
 }
