bd968ffa59439e82f72dba2376e1e2c70ee8ec2e
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-09-10 18:56:02 +0900

    third

--- kadai03/app/src/main/java/com/example/leapyear/MainActivity.kt
+++ kadai03/app/src/main/java/com/example/leapyear/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.leapyear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,26 +15,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.button)
+        var textView = findViewById<TextView>(R.id.textView)
 
         button.setOnClickListener {
             var isValid = true
-            val editTextNumber =  findViewById<EditText>(R.id.editText)
-            val EditText = editTextNumber.text.toString()
+            val editText = findViewById<EditText>(R.id.editText)
 
-            if(EditText.isEmpty()){
-                editTextNumber.error = getString(R.string.EditText_error)
+            if(editText.text.isEmpty()){
+                editText.error = getString(R.string.EditText_error)
                 isValid = false
             }
 
            if (isValid) {
-               val year = Integer.parseInt(EditText)
+               val year = editText.text.toString().toInt()
 
                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-                   val result = findViewById<TextView>(R.id.textView)
-                   result.text = getString(R.string.result, year)
-               } else {
-                   val result2 = findViewById<TextView>(R.id.textView)
-                   result2.text = getString(R.string.result2, year)
+                   textView.text = "$year is leap year"
+               } else{
+                   textView.text = "$year is not leap year"
                }
            }
         }
