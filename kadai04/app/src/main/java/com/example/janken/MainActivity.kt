de060cde14ae6fe5b01e9955db19fc15d49e2585
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-09-28 00:05:03 +0900

    add result file plus fix animation

--- kadai04/app/src/main/java/com/example/janken/MainActivity.kt
+++ kadai04/app/src/main/java/com/example/janken/MainActivity.kt
@@ -1,80 +1,46 @@
 package com.example.janken
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.drawable.AnimationDrawable
 import kotlinx.android.synthetic.main.activity_main.*
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import kotlin.random.Random
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var ad = AnimationDrawable()
-        var frame1 = resources.getDrawable(R.drawable.rock)
-        var frame2 = resources.getDrawable(R.drawable.scissors)
-        var frame3 = resources.getDrawable(R.drawable.paper)
+        var AnimationDrawable = AnimationDrawable()
+        var frame1 = ResourcesCompat.getDrawable ( resources,R.drawable.rock,null )
+        var frame2 = ResourcesCompat.getDrawable ( resources,R.drawable.scissors,null )
+        var frame3 = ResourcesCompat.getDrawable ( resources,R.drawable.paper,null )
 
-        ad.addFrame(frame1,50)
-        ad.addFrame(frame2,50)
-        ad.addFrame(frame3,50)
-        imageView.background = ad
-        ad.start()
-
-
-        rock.setOnClickListener{
-            janken(HandType.rock)
-            ad.stop()
+        if (frame1 != null) {
+            AnimationDrawable.addFrame ( frame1,50 )
+        }
+        if ( frame2 != null ) {
+            AnimationDrawable.addFrame ( frame2,50 )
+        }
+        if ( frame3 != null ) {
+            AnimationDrawable.addFrame (frame3,50 )
         }
 
-        scissors.setOnClickListener{
-            janken(HandType.scissors)
-            ad.stop()
-        }
+        enemyHand.background = AnimationDrawable
+        AnimationDrawable.start()
 
-        paper.setOnClickListener{
-            janken(HandType.paper)
-            ad.stop()
-        }
+        rock.setOnClickListener { onJankenBottonTapped ( it )}
+        scissors.setOnClickListener { onJankenBottonTapped ( it ) }
+        paper.setOnClickListener { onJankenBottonTapped ( it ) }
+
     }
 
-    enum class HandType(val n:Int){
-        rock(1),
-        scissors(2),
-        paper(3)
-    }
-
-    fun janken(hand : HandType){
-        val r = Random.nextInt(3)
-        val enemyHand:Int
-        val myHand:Int=hand.n
-
-        if (r == 0) {
-            enemyHand = HandType.rock.n
-            imageView.setImageResource(R.drawable.rock)
-        } else if (r == 1) {
-            enemyHand = HandType.scissors.n
-            imageView.setImageResource(R.drawable.scissors)
-        } else{
-            enemyHand = HandType.paper.n
-            imageView.setImageResource(R.drawable.paper)
-        }
-        when{
-            enemyHand == myHand ->{
-                textView.text = "You Drow"
-            }
-
-            ((enemyHand == 1) && (myHand==3)) || ((enemyHand == 2) && (myHand == 1)) || ((enemyHand == 3) && (myHand == 2)) ->{
-                textView.text = "You Win!"
-            }
-
-            else ->{
-                textView.text = "You Lose.."
-            }
-        }
+    fun onJankenBottonTapped( view: View? ) {
+        val intent = Intent( this, ResultActiviity::class.java )
+        intent.putExtra("myHand", view?.id)
+        startActivity( intent )
     }
 }
+
