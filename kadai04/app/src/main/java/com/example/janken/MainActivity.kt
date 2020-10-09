fc6aa5fb5ab74720df8a838baf3e42edcc095b4f
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-10-09 18:49:43 +0900

    修正途中です

--- kadai04/app/src/main/java/com/example/janken/MainActivity.kt
+++ kadai04/app/src/main/java/com/example/janken/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.janken
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.drawable.AnimationDrawable
-import android.os.Handler
+import android.view.animation.AlphaAnimation
 import kotlinx.android.synthetic.main.activity_main.*
-import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
-import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,70 +16,67 @@ class MainActivity : AppCompatActivity() {
         pa(2)
     }
 
+    private lateinit var jankenAnimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val animationDrawable = AnimationDrawable()
-        val frame1 = ResourcesCompat.getDrawable(resources, R.drawable.rock, null)
-        val frame2 = ResourcesCompat.getDrawable(resources, R.drawable.scissors, null)
-        val frame3 = ResourcesCompat.getDrawable(resources, R.drawable.paper, null)
-
-        if (frame1 != null) {
-            animationDrawable.addFrame(frame1, 100)
-        }
-        if (frame2 != null) {
-            animationDrawable.addFrame(frame2, 100)
-        }
-        if (frame3 != null) {
-            animationDrawable.addFrame(frame3, 100)
+        val jankenImage = findViewById<ImageView>(R.id.enemyHand).apply {
+            setBackgroundResource(R.drawable.animation_list)
+            jankenAnimation = background as AnimationDrawable
         }
 
-        enemyHand.background = animationDrawable
-        animationDrawable.start()
+        jankenImage.setOnClickListener({ jankenAnimation.start() })
 
-        rock.setOnClickListener {
-            janken(handType.gu)
-            onJankenBottonTapped( it )
-        }
+            rock.setOnClickListener {
+                bottonAnimation(rock)
+                janken(handType.gu)
+            }
 
-        scissors.setOnClickListener {
-            janken(handType.choki)
-            onJankenBottonTapped( it )
-        }
+            scissors.setOnClickListener {
+                bottonAnimation(scissors)
+                janken(handType.choki)
+            }
 
-        paper.setOnClickListener {
-            janken(handType.pa)
-            onJankenBottonTapped( it )
-        }
+            paper.setOnClickListener {
+                bottonAnimation(paper)
+                janken(handType.pa)
+
+            }
 
     }
-    fun janken(hand: handType) {
 
+    fun janken(hand: handType) {
         val myHand: Int = hand.n
         val cpHand = (Math.random() * 3).toInt()
 
-
-        when (cpHand) {
-            0 -> enemyHand.setImageResource(R.drawable.rock)
-            1 -> enemyHand.setImageResource(R.drawable.scissors)
-            2 -> enemyHand.setImageResource(R.drawable.paper)
+        when ( cpHand ) {
+            0 -> enemyHand.setImageResource ( R.drawable.rock)
+            1 -> enemyHand.setImageResource ( R.drawable.scissors)
+            2 -> enemyHand.setImageResource ( R.drawable.paper)
         }
 
-        val gameResult = (cpHand - myHand + 3) % 3
+        val gameResult = ( cpHand - myHand + 3) % 3
 
         when (gameResult) {
             0 -> textView.text = "You drow"
             1 -> textView.text = "You win!"
             2 -> textView.text = "You lose"
         }
-
     }
 
-    fun onJankenBottonTapped( view: View? ) {
-        val intent = Intent( this, MainActivity2::class.java )
-        intent.putExtra("myHand", view?.id)
-        startActivity( intent )
+    fun bottonAnimation( imageBotton:ImageButton ) {
+        val fadeInAnimation = AlphaAnimation(0.0f,1.0f)
+        val fadeOutAnimation = AlphaAnimation(0.0f,1.0f)
+
+        fadeOutAnimation.duration = 500
+        fadeOutAnimation.fillAfter = true
+        imageBotton.animation = fadeOutAnimation
+        fadeInAnimation.duration = 500
+        fadeInAnimation.fillAfter= true
+        imageBotton.animation = fadeInAnimation
+
     }
 }
-
