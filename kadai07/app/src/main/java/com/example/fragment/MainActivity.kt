bf0652ada49a58cb35b3adb56ebfbc8a3dca4bf6
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-11-17 18:02:43 +0900

    課題７を作りました。

---
+++ kadai07/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fragment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
