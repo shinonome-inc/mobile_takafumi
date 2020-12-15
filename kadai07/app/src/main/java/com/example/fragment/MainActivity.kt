c867e0a6ea0c1c02146f8aae276434abe9c57f60
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2020-12-15 22:07:45 +0900

    Fragmentについて調べて修正しました。

--- kadai07/app/src/main/java/com/example/fragment/MainActivity.kt
+++ kadai07/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -3,16 +3,38 @@ package com.example.fragment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.first_fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        var isFragment: Boolean = false
+        val fragmentManager = supportFragmentManager
+
+        button.setOnClickListener() {
+            if (isFragment == false) {
+                val fragmentTransaction = fragmentManager.beginTransaction()
+
+                fragmentTransaction.add(R.id.container, FirstFragment(), "test")
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
+                isFragment = true
+            } else {
+                val fragmentTransaction = fragmentManager.beginTransaction()
+
+                fragmentManager.findFragmentByTag("test")?.let{
+                    fragmentTransaction.remove(it)
+                }
+
+                fragmentTransaction.commit()
+                isFragment = false
+            }
         }
+
     }
 }
