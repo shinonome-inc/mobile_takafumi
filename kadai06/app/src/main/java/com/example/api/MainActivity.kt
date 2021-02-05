976371e64c78a1c5609039a5f7d7db02cb4aff8c
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2021-02-05 15:12:58 +0900

    layout修正

--- kadai06/app/src/main/java/com/example/api/MainActivity.kt
+++ kadai06/app/src/main/java/com/example/api/MainActivity.kt
@@ -10,17 +10,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://qiita.com/api/v2/docs")
+            .baseUrl("http://qiita.com/api/v2")
             .addConverterFactory(GsonConverterFactory.create())
             //上はGsonのファクトリーメソッドに必須
             .build()
 
         val itemService = retrofit.create(ItemService::class.java)
+
         itemService.items(1, 20)?.enqueue(object: Callback<List<ItemEntity?>?> {
             override fun onFailure(call: Call<List<ItemEntity?>?>, t: Throwable){
                 Log.d("Error", t.message.toString())
