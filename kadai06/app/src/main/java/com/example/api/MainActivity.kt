482da57444d0948175086d5ae941ca39bca4241c
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2021-02-07 21:09:46 +0900

    インターネット確認

--- kadai06/app/src/main/java/com/example/api/MainActivity.kt
+++ kadai06/app/src/main/java/com/example/api/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+    
         val retrofit = Retrofit.Builder()
             .baseUrl("http://qiita.com/api/v2/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<ItemEntity?>?>, response: Response<List<ItemEntity?>?>) {
                 Log.d("Successful", response.body().toString())
             }
-    
+
         })
     }
 }
