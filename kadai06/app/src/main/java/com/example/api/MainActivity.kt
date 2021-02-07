2eb94c8944a50c8f5807e824bd621ce020ccb402
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2021-02-08 00:08:53 +0900

    1度提出します

--- kadai06/app/src/main/java/com/example/api/MainActivity.kt
+++ kadai06/app/src/main/java/com/example/api/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    
+
         val retrofit = Retrofit.Builder()
             .baseUrl("http://qiita.com/api/v2/")
             .addConverterFactory(GsonConverterFactory.create())
