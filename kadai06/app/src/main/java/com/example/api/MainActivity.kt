89fc3e0d7c9e3b3acde2cbd474da793b5ea766a6
taka0815basket <66946792+taka0815basket@users.noreply.github.com>
2021-02-08 18:34:12 +0900

    APIで取得したデータをtextviewで表示できるようにしたのと、レイアウトを作ってみました。

--- kadai06/app/src/main/java/com/example/api/MainActivity.kt
+++ kadai06/app/src/main/java/com/example/api/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.api
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,25 +13,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://qiita.com/api/v2/")
+        .addConverterFactory(GsonConverterFactory.create())
+        //上はGsonのファクトリーメソッドに必須
+        .build()
+
+    val itemService = retrofit.create(ItemService::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://qiita.com/api/v2/")
-            .addConverterFactory(GsonConverterFactory.create())
-            //上はGsonのファクトリーメソッドに必須
-            .build()
-
-        val itemService = retrofit.create(ItemService::class.java)
-
-        itemService.items(1, 20)?.enqueue(object: Callback<List<ItemEntity?>?> {
-            override fun onFailure(call: Call<List<ItemEntity?>?>, t: Throwable){
+        itemService.items(1, 20)?.enqueue(object : Callback<List<ItemEntity?>?> {
+            override fun onFailure(call: Call<List<ItemEntity?>?>, t: Throwable) {
                 Log.d("Error", t.message.toString())
             }
 
-            override fun onResponse(call: Call<List<ItemEntity?>?>, response: Response<List<ItemEntity?>?>) {
-                Log.d("Successful", response.body().toString())
+            override fun onResponse(
+                call: Call<List<ItemEntity?>?>,
+                response: Response<List<ItemEntity?>?>
+            ) {
+                text1.text = response.body().toString()
             }
 
         })
